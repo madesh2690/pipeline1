2115465887cef74ab20fc286aa6a660244aefc39
madesh2690 <madeshdc@gmail.com>
2019-03-21 21:43:28 +0000

    Update masterJob.groovy

--- masterJob.groovy
+++ masterJob.groovy
@@ -8,7 +8,7 @@ pipeline {
      stage ('Build'){
        steps {
           sh "echo ${appName}"
-         build(job: 'Madesh-Test-Pipeline', parameters: [[$class: 'StringParameterValue', name: 'P1', value: 'AAAAA'], [$class: 'StringParameterValue', name: 'P2', value: 'BBBB']])
+         build(job: 'Madesh-FreeStyle-Test', parameters: [[$class: 'StringParameterValue', name: 'P1', value: 'AAAAA'], [$class: 'StringParameterValue', name: 'P2', value: 'BBBB']])
          //userInput = input( id: 'Procced 2', message: 'Enter panameter', parameters: [$class: 'ChoiceParameterDefinition', name: "ENV_NAME", choices: "madesh", description: "selcte my name"])
                }
          }
