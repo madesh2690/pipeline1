d10855c16ffba89fedc74143ce492ab84fa77c4d
madesh2690 <madeshdc@gmail.com>
2019-03-23 00:38:04 +0000

    Update masterJob.groovy

--- masterJob.groovy
+++ masterJob.groovy
@@ -11,7 +11,7 @@ pipeline {
      stage ('Build'){
        steps {
           sh "echo ${appName}"
-         def userInput = input( id: 'userInput', message: 'Enetr User Inputs', parameters: [$class: 'ChoiceParameterDefinition', name: 'ami', choices: 'Linux', description: 'AMI'])
+         def userInput = input( id: 'userInput', message: 'Enetr User Inputs', parameters: [$class: 'ChoiceParameterDefinition', name: 'ami', choices: Linux, description: 'AMI'])
          //build(job: 'Madesh-FreeStyle-Test', parameters: [[name: 'P1', value: '${params.Nodes}'], [$class: 'StringParameterValue', name: 'P2', value: 'BBBB']])
          //userInput = input( id: 'Procced 2', message: 'Enter panameter', parameters: [$class: 'ChoiceParameterDefinition', name: "ENV_NAME", choices: "madesh", description: "selcte my name"])
                }
