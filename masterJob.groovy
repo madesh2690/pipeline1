bedf527e04facaef4fc2ed84f36fb385ed75b3a2
madesh2690 <madeshdc@gmail.com>
2019-03-23 00:35:44 +0000

    Update masterJob.groovy

--- masterJob.groovy
+++ masterJob.groovy
@@ -5,13 +5,13 @@ appName="CLK"
 pipeline {
   agent {label "master"}
   parameters {
-    choice ( name: 'Nodes', choices: "Linux\nMac", description: "Chosee Nodes!")
+    //choice ( name: 'Nodes', choices: "Linux\nMac", description: "Chosee Nodes!")
           }
             stages {
      stage ('Build'){
        steps {
           sh "echo ${appName}"
-         def userInput = input( id: 'userInput', message: 'Enetr User Inputs', parameters: [$class: 'ChoiceParameterDefinition', name: 'ami', choices: 'Linux' description: 'AMI'])
+         def userInput = input( id: 'userInput', message: 'Enetr User Inputs', parameters: [$class: 'ChoiceParameterDefinition', name: 'ami', choices: 'Linux', description: 'AMI'])
          //build(job: 'Madesh-FreeStyle-Test', parameters: [[name: 'P1', value: '${params.Nodes}'], [$class: 'StringParameterValue', name: 'P2', value: 'BBBB']])
          //userInput = input( id: 'Procced 2', message: 'Enter panameter', parameters: [$class: 'ChoiceParameterDefinition', name: "ENV_NAME", choices: "madesh", description: "selcte my name"])
                }
